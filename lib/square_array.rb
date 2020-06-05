864ee594fe86589b62901f832966b5910a05821c
imperative-publisher-3811 <mcasimirian@gmail.com>
2020-06-05 19:38:10 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,7 +1,7 @@
 def square_array(numbers)
   new_array = []
   while numbers.length do |number|
-    new_array << number ** 2
+    new_array << |number| ** 2
   end
   p new_array # your code herexs
 end
