232465ca8a6e431dda6b5ec36d8484f649322190
imperative-publisher-3811 <mcasimirian@gmail.com>
2020-06-05 22:45:28 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,9 @@
-def square_array(array)
-  # your code here
+def square_array(numbers)
+  new_array = []
+  index = 0
+  while index < numbers.length do
+    new_array << numbers[index] * numbers[index]
+    index += 1
+  end
+  p new_array # your code herexs
 end
